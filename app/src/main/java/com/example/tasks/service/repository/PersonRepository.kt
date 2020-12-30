96013ed389f13834cf5993bbe5422766b854bddd
Victor Amaral <vic_amaral_23@hotmail.com>
2020-12-30 18:44:20 -0300

    Implementa serviço de comunicação com a API usando o Retrofit.

--- app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
+++ app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
@@ -1,14 +1,28 @@
 package com.example.tasks.service.repository
 
+import com.example.tasks.service.HeaderModel
 import com.example.tasks.service.repository.remote.PersonService
 import com.example.tasks.service.repository.remote.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class PersonRepository {
 
-    val remote = RetrofitClient.createService(PersonService::class.java)
+    private val remote = RetrofitClient.createService(PersonService::class.java)
 
     fun login(email: String, password: String) {
-        remote.login(email, password)
+        val call: Call<HeaderModel> = remote.login(email, password)
+        call.enqueue(object : Callback<HeaderModel> {
+            override fun onResponse(call: Call<HeaderModel>, response: Response<HeaderModel>) {
+                val header = response.body()
+            }
+
+            override fun onFailure(call: Call<HeaderModel>, t: Throwable) {
+                val s = ""
+            }
+
+        })
     }
 
 }
--- app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
@@ -2,13 +2,14 @@ package com.example.tasks.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.example.tasks.service.repository.PersonRepository
 
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
-    /**
-     * Faz login usando API
-     */
+    private val personRepository = PersonRepository()
+
     fun doLogin(email: String, password: String) {
+        personRepository.login(email, password)
     }
 
     /**
