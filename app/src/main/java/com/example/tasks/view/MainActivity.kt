420c5dd4a790af4d3f9713d1d70a8fdafcff44df
Victor Amaral <vic_amaral_23@hotmail.com>
2021-02-14 00:42:17 -0300

    Adiciona o calendário na TaskFormActivity.kt

--- app/src/main/java/com/example/tasks/view/MainActivity.kt
+++ app/src/main/java/com/example/tasks/view/MainActivity.kt
@@ -1,20 +1,20 @@
 package com.example.tasks.view
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.lifecycle.ViewModelProvider
 import com.example.tasks.R
 import com.example.tasks.viewmodel.MainViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,8 +32,7 @@ class MainActivity : AppCompatActivity() {
 
         val fab: FloatingActionButton = findViewById(R.id.fab)
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            startActivity(Intent(this, TaskFormActivity::class.java))
         }
 
         setupNavigation()
--- app/src/main/java/com/example/tasks/view/TaskFormActivity.kt
+++ app/src/main/java/com/example/tasks/view/TaskFormActivity.kt
@@ -1,16 +1,24 @@
 package com.example.tasks.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.View
+import android.widget.DatePicker
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.tasks.R
 import com.example.tasks.viewmodel.RegisterViewModel
 import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.android.synthetic.main.activity_register.button_save
+import kotlinx.android.synthetic.main.activity_task_form.*
+import java.text.SimpleDateFormat
+import java.util.*
 
-class TaskFormActivity : AppCompatActivity(), View.OnClickListener {
+class TaskFormActivity : AppCompatActivity(), View.OnClickListener,
+    DatePickerDialog.OnDateSetListener {
 
     private lateinit var viewModel: RegisterViewModel
+    private val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,15 +31,25 @@ class TaskFormActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View) {
-        val id = v.id
-        if (id == R.id.button_save) {
+        when (v.id) {
+            R.id.button_save -> {
+                val name = edit_name.text.toString()
+                val email = edit_email.text.toString()
+                val password = edit_password.text.toString()
 
-            val name = edit_name.text.toString()
-            val email = edit_email.text.toString()
-            val password = edit_password.text.toString()
+                viewModel.create(name, email, password)
+            }
 
-            viewModel.create(name, email, password)
+            R.id.button_date -> showDatePicker()
         }
+    }
+
+    private fun showDatePicker() {
+        val userCalendar = Calendar.getInstance()
+        val year = userCalendar.get(Calendar.YEAR)
+        val month = userCalendar.get(Calendar.MONTH)
+        val day = userCalendar.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(this, this, year, month, day).show()
     }
 
     private fun observe() {
@@ -39,6 +57,17 @@ class TaskFormActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun listeners() {
         button_save.setOnClickListener(this)
+        button_date.setOnClickListener(this)
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+
+        val newCalendar = Calendar.getInstance()
+        newCalendar.set(year, month, dayOfMonth)
+
+        val formatedCalendar = dateFormat.format(newCalendar.time)
+        button_date.text = formatedCalendar
+
     }
 
 }
