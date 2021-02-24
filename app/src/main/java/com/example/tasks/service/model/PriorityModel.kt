c73c0f0527bf5a1b9cdd2085ee4269c8eaeb95d3
Victor Amaral <vic_amaral_23@hotmail.com>
2021-02-23 22:40:11 -0300

    Corrige erro no PriorityModel.kt

--- app/src/main/java/com/example/tasks/service/model/PriorityModel.kt
+++ app/src/main/java/com/example/tasks/service/model/PriorityModel.kt
@@ -3,9 +3,18 @@ package com.example.tasks.service.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
 
-@Entity(tableName = "priority")
-data class PriorityModel(
-    @PrimaryKey @ColumnInfo(name = "id") val id: Int,
-    @ColumnInfo(name = "description") val description: String
-)
+@Entity(tableName = "Priority")
+class PriorityModel {
+
+    @SerializedName("Id")
+    @ColumnInfo(name = "id")
+    @PrimaryKey
+    var id: Int = 0
+
+    @SerializedName("Description")
+    @ColumnInfo(name = "description")
+    lateinit var description: String
+
+}
