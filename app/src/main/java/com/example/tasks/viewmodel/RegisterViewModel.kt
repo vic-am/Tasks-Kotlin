6b554beab71bda05493692001f047e957438226e
Victor Amaral <vic_amaral_23@hotmail.com>
2021-04-08 23:38:47 -0300

    Corrige um erro quando um novo usuário é criado

--- app/src/main/java/com/example/tasks/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/example/tasks/viewmodel/RegisterViewModel.kt
@@ -4,12 +4,13 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.example.tasks.service.model.HeaderModel
+import com.example.tasks.service.constants.TaskConstants.SHARED.PERSON_EMAIL
 import com.example.tasks.service.constants.TaskConstants.SHARED.PERSON_KEY
 import com.example.tasks.service.constants.TaskConstants.SHARED.PERSON_NAME
 import com.example.tasks.service.constants.TaskConstants.SHARED.TOKEN_KEY
 import com.example.tasks.service.listener.ApiListener
 import com.example.tasks.service.listener.ValidationListener
+import com.example.tasks.service.model.HeaderModel
 import com.example.tasks.service.repository.PersonRepository
 import com.example.tasks.service.repository.local.SecurityPreferences
 
@@ -27,6 +28,7 @@ class RegisterViewModel(application: Application) : AndroidViewModel(application
                 sharedPreferences.store(TOKEN_KEY, model.token)
                 sharedPreferences.store(PERSON_KEY, model.personKey)
                 sharedPreferences.store(PERSON_NAME, model.name)
+                sharedPreferences.store(PERSON_EMAIL, email)
 
                 mutableCreate.value = ValidationListener()
             }
